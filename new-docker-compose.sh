a731eb03115ebe5250d921fce897a63ded08dd5f
P Sandeep <87634151+PolleSandeep@users.noreply.github.com>
2025-12-24 15:57:14 +0530

    Create new-docker-compose.sh

---
+++ new-docker-compose.sh
@@ -0,0 +1,8 @@
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -L https://github.com/docker/compose/releases/download/v5.0.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+#Buildx installation:-
+sudo mkdir -p /usr/libexec/docker/cli-plugins
+sudo curl -L https://github.com/docker/buildx/releases/download/v0.17.1/buildx-v0.17.1.linux-amd64 \ -o /usr/libexec/docker/cli-plugins/docker-buildx
+sudo chmod +x /usr/libexec/docker/cli-plugins/docker-buildx
