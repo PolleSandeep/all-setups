0fd370426b2446ad9188be8f4057f842e3d0165c
P Sandeep <87634151+PolleSandeep@users.noreply.github.com>
2026-01-18 12:46:20 +0530

    Create kops-linux.sh

---
+++ kops-linux.sh
@@ -0,0 +1,18 @@
+#Vim .bashrc
+#export PATH=$PATH:/usr/local/bin/
+#source .bashrc 
+
+#install aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+#install kubetcl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+mv kubectl /usr/local/bin/
+
+#install kops
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
