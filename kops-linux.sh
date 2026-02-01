2c7d77512f3121468695096ca049b4875533f1f0
P Sandeep <87634151+PolleSandeep@users.noreply.github.com>
2026-02-01 14:05:17 +0530

    Update kops-linux.sh

--- kops-linux.sh
+++ kops-linux.sh
@@ -16,3 +16,6 @@ mv kubectl /usr/local/bin/
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops
 sudo mv kops /usr/local/bin/kops
+
+export KOPS_STATE_STORE=s3://sandeep298.flm
+kops create cluster --name sandeep.k8s.local --zones us-east-1a,us-east-1b --master-count 1 --master-size t2.medium --master-volume-size 25 --node-count 2 --node-size t2.micro --node-volume-size 20
